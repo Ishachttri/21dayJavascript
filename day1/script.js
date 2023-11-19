76d4e3a296febe55114fb36c3b5fa32b29383af9
Isha kandel <ishachttri@gmail.com>
2023-11-19 22:04:02 +0545

    innnnnnnnntriduction of javascript

--- day1/script.js
+++ day1/script.js
@@ -1,3 +1,21 @@
 alert("hello world");
 
 console.log("hey i sm console");
+console.log("hey i sm ");
+console.log("hey");
+ 
+var a =prompt("Enter your number")
+var isTrue =confirm("Are you sure you want to leave this page and blast your computer")
+if("isTrue")
+{
+    console.log("computer is lasting")
+}
+else{
+    console.log("computer is not blasting")
+}
+
+
+
+
+console.log("your number is" +a)
+
