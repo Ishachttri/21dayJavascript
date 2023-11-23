dcdcb16312f308b6bc8ca4bb3c0b29b089831364
Isha kandel <ishachttri@gmail.com>
2023-11-23 23:41:00 +0545

    learining variable and object

--- day3/script.js
+++ day3/script.js
@@ -1,11 +1,32 @@
 console.log("this is my 3rd js program");
 // var is not invalid in javascript
-var a=4;
-var b=6
+var a = 4;
+var b = 6
 console.log("a+b");
 // used let insted of var
-let a=6;
-let b=4;
-let c=("isha");
-let d=("goley";)
+let a = 6;
+let b = 4;
+let c = ("isha");
+let d = ("goley");
 console.loglog("a+b");
+
+let x = ("isha");
+let y = 6;
+let z = 4;
+const p = true;
+let q = undefined;
+let r = null;
+
+console.log(x,y,z,p,q,r);
+
+let o = {
+    "name":"harry";
+    "job code":400
+
+    console.log(o);
+    o.salary= "100crores";
+
+
+
+
+}
