1ec9757ea6ae03f3a0ba38e1fd694d994b9fba2c
Isha kandel <ishachttri@gmail.com>
2023-11-23 23:32:48 +0545

    usimg let

--- day3/script.js
+++ day3/script.js
@@ -3,4 +3,9 @@ console.log("this is my 3rd js program");
 var a=4;
 var b=6
 console.log("a+b");
-console.loglog("")
+// used let insted of var
+let a=6;
+let b=4;
+let c=("isha");
+let d=("goley";)
+console.loglog("a+b");
