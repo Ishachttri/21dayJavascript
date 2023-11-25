d8bec99b02a0a3748f46dedbc047456a6b904be5
Isha kandel <ishachttri@gmail.com>
2023-11-25 22:57:11 +0545

    lerning if else

---
+++ day4/index.js
@@ -0,0 +1,15 @@
+console.log("hello i am extension)")
+
+let age=17;
+let grace=2;
+
+console.log(age + grace)
+
+
+if (age>18){
+    console.log("you can drive");
+
+}
+else{
+    console.log("you cannot drive")
+}
