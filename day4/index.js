df72c1b7afb9a9b27476496490b57650d68ea385
Isha kandel <ishachttri@gmail.com>
2023-11-25 23:07:18 +0545

    ifelseif ladder

--- day4/index.js
+++ day4/index.js
@@ -1,21 +1,20 @@
-console.log("hello i am extension)")
+console.log("hello i am extension)");
 
-let age=17;
-let grace=2;
-age += grace
-console.log(age)
-console.log(age + grace)
-console.log(age - grace)
-console.log(age *grace)
-console.log(age / grace)
-console.log(age ** grace)
-console.log(age % grace)
+let age = 17;
+// let grace=2;
+// age += grace
+// console.log(age)
+// console.log(age + grace)
+// console.log(age - grace)
+// console.log(age *grace)
+// console.log(age / grace)
+// console.log(age ** grace)
+// console.log(age % grace)
 
-if (age>18){
-    console.log("you can drive");
-
+if (age != 18) {
+  console.log("you can drive");
+} else if (age == 0) {
+  console.log("are you kidding");
+} else {
+  console.log("you cannot drive");
 }
-else{
-console.log("you cannot drive")
-}
-
