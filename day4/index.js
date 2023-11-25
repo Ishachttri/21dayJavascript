3783416d78fad9ac9e86c18a8319f8d23401a6e0
Isha kandel <ishachttri@gmail.com>
2023-11-25 23:00:44 +0545

    operator

--- day4/index.js
+++ day4/index.js
@@ -2,14 +2,20 @@ console.log("hello i am extension)")
 
 let age=17;
 let grace=2;
-
+age += grace
+console.log(age)
 console.log(age + grace)
-
+console.log(age - grace)
+console.log(age *grace)
+console.log(age / grace)
+console.log(age ** grace)
+console.log(age % grace)
 
 if (age>18){
     console.log("you can drive");
 
 }
 else{
-    console.log("you cannot drive")
+console.log("you cannot drive")
 }
+
